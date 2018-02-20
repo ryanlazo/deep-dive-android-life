540ad0c8ed92e9bd8a6aafd13be1fac85ebeaeb7
ryanlazo <ryanlazo12@gmail.com>
2018-02-20 18:07:12 -0500

    Conway Android app

--- app/src/main/java/edu/cnm/deepdive/life/MainActivity.java
+++ app/src/main/java/edu/cnm/deepdive/life/MainActivity.java
@@ -1,7 +1,10 @@
 package edu.cnm.deepdive.life;
 
+import android.os.Looper;
+import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.widget.SeekBar;
@@ -11,7 +14,6 @@ import edu.cnm.deepdive.life.ca.Model;
 public class MainActivity extends AppCompatActivity {
 
   private Model model;
-  final private Object lock = new Object();
   private boolean running = false;
   private Runner runner = null;
   private TextView generationCounter;
@@ -76,6 +78,7 @@ public class MainActivity extends AppCompatActivity {
 
   @Override
   public boolean onPrepareOptionsMenu(Menu menu) {
+    Log.d(getClass().getName(), "entering onPrepareOptionsMenu");
     menu.findItem(R.id.action_step).setEnabled(!running);
     menu.findItem(R.id.action_run).setVisible(!running);
     menu.findItem(R.id.action_stop).setVisible(running);
@@ -83,37 +86,88 @@ public class MainActivity extends AppCompatActivity {
     return true;
   }
 
-  private void updateView(byte[][] terrain, int generation) {
-
+  private void updateView(byte[][] terrain, final int generation) {
     terrainView.setTerrain(terrain);
-    terrainView.invalidate();
-    generationCounter.setText(String.format(generationFormat, generation));
+    if (Looper.getMainLooper().getThread()== Thread.currentThread()) {
+      terrainView.invalidate();
+      generationCounter.setText(String.format(generationFormat, generation));
+    } else {
+      terrainView.postInvalidate();
+      runOnUiThread(new Runnable() {
+        @Override
+          public void run() {
+          generationCounter.setText(String.format(generationFormat, generation));
+        }
+      });
+    }
+
 
   }
+
+  @Override
+  protected void onPostCreate(@Nullable Bundle savedInstanceState) {
+    super.onPostCreate(savedInstanceState);
+  }
+
+  @Override
+  protected void onPostResume() {
+    super.onPostResume();
+  }
+
+  @Override
+  protected void onStart() {
+    super.onStart();
+  }
+
+  @Override
+  protected void onStop() {
+    super.onStop();
+  }
+
+  @Override
+  protected void onDestroy() {
+    super.onDestroy();
+  }
+
+  @Override
+  protected void onSaveInstanceState(Bundle outState) {
+    super.onSaveInstanceState(outState);
+  }
+
+  @Override
+  protected void onPause() {
+    super.onPause();
+  }
+
+  @Override
+  protected void onResume() {
+    super.onResume();
+  }
+
+  @Override
+  protected void onRestart() {
+    super.onRestart();
+  }
+
   private class Runner extends Thread {
 
     @Override
     public void run() {
       while (running) {
-        synchronized (lock) {
           model.advance();
           if (!terrainView.isUpdatePending()) {
             update();
           }
-        }
+
       }
       update();
     }
 
     private void update() {
-      final byte[][] terrain = model.getTerrain();
-      final int generation = model.getGeneration();
-      runOnUiThread(new Runnable() {
-        @Override
-        public void run() {
-          updateView(terrain, generation);
-        }
-      });
+     byte[][] terrain = model.getTerrain();
+     int generation = model.getGeneration();
+      updateView(terrain, generation);
+
     }
 
   }
